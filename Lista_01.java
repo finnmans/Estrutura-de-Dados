0d54d0bba7121d8456435dc227b17b8cf0937a6d
finnmans <57464711+finnmans@users.noreply.github.com>
2022-03-08 01:51:58 -0300

    Factorial

--- Lista_01.java
+++ Lista_01.java
@@ -2,13 +2,25 @@ class main{
 
 
     public static int fibonacci(int position) {
-            if(position <= 1) return position;
-                return fibonacci(position - 1) + fibonacci(position - 2);
+        if(position <= 1) return position;
+           else return fibonacci(position - 1) + fibonacci(position - 2);
 
-        }
+ }
+
+
+ public static Integer factorial(Integer value) {
+
+    if(value <= 1) return value;
+       return value  * factorial(value - 1);
+
+  }
+  public static String toString(Integer inputValue, Integer result) {
+    return inputValue + "! = "  + result;
+  }
 
     public static void main(String[] args) {
-       System.out.println(fibonacci(4));
+
+       System.out.println(factorial(3));
     }
 
 }
