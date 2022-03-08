61c77a6b7051b16ad919ebfb000eb8d7d0c58879
finnmans <57464711+finnmans@users.noreply.github.com>
2022-03-08 13:05:32 -0300

    FUNCIONOU MIZERA
    ESSA STRING NAO TEM MAIS PI AE POHA

--- Lista_01.java
+++ Lista_01.java
@@ -1,26 +1,62 @@
 class main{
 
-
+//Fibonacci Recursivo
     public static int fibonacci(int position) {
         if(position <= 1) return position;
            else return fibonacci(position - 1) + fibonacci(position - 2);
 
  }
 
-
+//Factorial Recursivo
  public static Integer factorial(Integer value) {
 
-    if(value <= 1) return value;
-       return value  * factorial(value - 1);
+    if(value == 1) return value;
+        else if(value < 1) return value = 1;
+     return value  * factorial(value - 1);
 
   }
+
+
   public static String toString(Integer inputValue, Integer result) {
     return inputValue + "! = "  + result;
   }
 
+  //Substituição em String por Recursividade
+
+  public static String recursiveSwap(String s) {
+
+        String pi = "3.14";
+         s = s.toLowerCase();     
+          s = s.replace(" ","");
+            s = s.replace(",","");
+              s = s.replace(".","");
+              
+         if(!s.contains("pi")) return s;
+          else if(s.charAt(0) == 'p' && s.charAt(1) == 'i'){
+
+           return pi + recursiveSwap(s.substring(2));
+         }
+            
+            return s.charAt(0) + recursiveSwap(s.substring(1));
+        
+         /*for(int i = 0; i != s.length(); i++){'
+        
+          if(s.charAt(i) == 'p' && s.charAt(i+1) == 'i'){
+            i++;
+            novaS = novaS + pi;
+          }
+           else novaS += s.charAt(i);
+      }*/
+      
+
+        
+   
+}//https://docs.oracle.com/javase/tutorial/java/data/manipstrings.html
     public static void main(String[] args) {
 
-       System.out.println(factorial(3));
+       System.out.println(recursiveSwap("pitopilomeu"));
+       System.out.println(recursiveSwap("ApIa"));
+       System.out.println(recursiveSwap(" a  , p .. i a"));
     }
 
 }
