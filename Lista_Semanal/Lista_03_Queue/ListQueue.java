263bdccdc6b1ca664bf16c78f7076a1433c8f16a
Felipe Vasconcelos <fsv2@ecomp.poli.br>
2022-08-05 01:25:05 -0300

    filas -_-

---
+++ Lista_Semanal/Lista_03_Queue/ListQueue.java
@@ -0,0 +1,43 @@
+package Lista_Semanal.Lista_03_Queue;
+
+public class ListQueue<T> {
+
+    Node<T> first;
+    Node<T> last;
+    int size = 0;
+
+    public void add(T data){
+        Node<T> newOne = new Node<T>(data);
+
+        if(first == null) first = last = newOne;
+        else{
+             last.next = last;
+              last = newOne;
+        }
+        size++;
+    }
+
+    public T dequeue(){
+        T r = null;
+        if(first != null){
+            r = first.data;
+            first = first.next;
+            if(first == null)  last = null;
+              size--;
+        }
+        return r;
+    }
+    public T head() {
+		T r = null;
+		if (first != null) {
+			r = first.data;
+		}
+		return r;
+	}
+
+	public int length() {
+		return size;
+	}
+
+
+}
---
+++ Lista_Semanal/Lista_03_Queue/Node.java
@@ -0,0 +1,11 @@
+package Lista_Semanal.Lista_03_Queue;
+
+public class Node<T> {
+    T data;
+    Node<T> next;
+
+    Node(T data){
+        this.data = data;
+        next = null;
+    }
+}
